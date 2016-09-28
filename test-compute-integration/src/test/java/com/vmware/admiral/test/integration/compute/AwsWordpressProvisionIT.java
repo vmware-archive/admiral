07347e6598761e7f5d8977b429b57ef5a1db48c1
Ilia Pantchev <ipantchev@vmware.com>
2016-09-28 13:50:41 +0300

    Fix checkstyle and findbugs errors
    Bug Number:
    Documentation Notes:
    PrecheckinTest URL: <required>
    QA Notes:
    Review URL: <required>
    Reviewed by: <required>
    Testing Done: <required>
    
    Change-Id: I906332f9d24f8479fae88c096c02b306acc1430f
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/2585
    Reviewed-by: Peter Mitrov <pmitrov@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>

--- test-compute-integration/src/test/java/com/vmware/admiral/test/integration/compute/AwsWordpressProvisionIT.java
+++ test-compute-integration/src/test/java/com/vmware/admiral/test/integration/compute/AwsWordpressProvisionIT.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
 package com.vmware.admiral.test.integration.compute;
 
 import static org.junit.Assert.assertNotNull;
@@ -77,6 +88,10 @@ public class AwsWordpressProvisionIT extends BaseComputeProvisionIT {
                 wordPress = computeState;
                 break;
             }
+        }
+
+        if (wordPress == null) {
+            fail("Unable to find the ComputeState corresponding to the Wordpress node");
         }
 
         String address = wordPress.address;
