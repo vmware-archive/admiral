3ea813108bd1c6585370cbb8d8c2cb24f239dca5
Lazarin Lazarov <llazarov@vmware.com>
2016-11-07 18:27:23 +0200

    Change date time log formatter
    Use Java 8 time api and change from SimpleDateFormat (SDF) to DateTimeFormatter (DTF).
    SDF is not thread safe and is constructed for each message.
    DTF is thread safe and is constructed only once.
    Internally SDF initializes and uses Calendar instance, much slower than DTF implementation.
    All that leads to 2.5 faster execution with DTF and is much less memory (and GC) intensive.
    
    Change-Id: I85ecc63933e331b00aafb9e8621a95a56ad562c1
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/3856
    Reviewed-by: Zahari Ivanov <zaharii@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>

--- common/src/main/java/com/vmware/admiral/common/AdmiralLogFormatter.java
+++ common/src/main/java/com/vmware/admiral/common/AdmiralLogFormatter.java
@@ -11,37 +11,59 @@
 
 package com.vmware.admiral.common;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.LogRecord;
 
 import com.vmware.xenon.common.LogFormatter;
+import com.vmware.xenon.common.Utils;
 
 public class AdmiralLogFormatter extends LogFormatter {
 
     private static final String LOGGING_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+    private static final DateTimeFormatter FORMATTER;
+
+    static {
+        DateTimeFormatter f = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        try {
+            f = DateTimeFormatter.ofPattern(LOGGING_FORMAT);
+        } catch (Exception e) {
+            Utils.logWarning("Cannot use pattern '%s' for date time formatting, "
+                            + "fallback to ISO_LOCAL_DATE_TIME (ISO-8601). "
+                            + "Error: %s",
+                    LOGGING_FORMAT, e.getMessage());
+        }
+        FORMATTER = f;
+    }
 
     @Override
     public String format(LogRecord record) {
         StringBuilder sb = new StringBuilder();
 
         LogItem log = LogItem.create(record);
-        sb.append("[").append(log.id).append("]");
-        sb.append("[").append(log.l.substring(0, 1)).append("]");
-        sb.append("[").append(getLocalizedDate(log.t)).append("]");
-        sb.append("[").append(log.classOrUri).append("]");
-        sb.append("[").append(log.method).append("]");
+        sb.append('[').append(log.id).append(']');
+        sb.append('[').append(log.l.substring(0, 1)).append(']');
+        sb.append('[').append(getFormattedDate(log.t)).append(']');
+        sb.append('[').append(log.classOrUri).append(']');
+        sb.append('[').append(log.method).append(']');
         if (log.m != null && !log.m.isEmpty()) {
-            sb.append("[").append(log.m).append("]");
+            sb.append('[').append(log.m).append(']');
         }
-        sb.append("\n");
+        sb.append('\n');
         return sb.toString();
     }
 
-    private Object getLocalizedDate(long t) {
-        DateFormat dateTimeFormat = new SimpleDateFormat(LOGGING_FORMAT);
-        Date date = new Date(t);
-        return dateTimeFormat.format(date);
+    /**
+     * Formats milliseconds to human readable date time string
+     *
+     * @param t milliseconds, returned by <code>System.currentTimeMillis()</code>
+     * @return formatted date time string
+     */
+    private String getFormattedDate(long t) {
+        long seconds = t / 1000;
+        int nanos = (int) (t % 1000) * 1_000_000;
+
+        return FORMATTER.format(LocalDateTime.ofEpochSecond(seconds, nanos, ZoneOffset.UTC));
     }
 }
