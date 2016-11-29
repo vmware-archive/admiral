71a680ddeee7884fe5a931e835fa4d0a7fc24634
Iveta Ilieva <iilieva@vmware.com>
2016-11-29 15:44:31 +0200

    Small style fixes.
    Change-Id: Ic20456bcacd1388c56f9e5aae4341f72f8b03c3f
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/4473
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>

--- ui/app/src/js/components/common/VueNavigationLink.js
+++ ui/app/src/js/components/common/VueNavigationLink.js
@@ -10,7 +10,8 @@
  */
 
 var VueNavigationLink = Vue.extend({
-  template: `<span class="navigation-link-holder" v-show="show" title="{{tooltip}}">
+  template: `<span class="navigation-link-holder" v-show="show" title="{{tooltip}}"
+                    :class="{'lone': lone}">
       <a class="navigation-link" href="{{link}}"><img v-if="iconName" class="nav-item-image"
           v-bind:src="iconName"> {{label}}</a>&nbsp;</span>`,
   props: {
@@ -35,6 +36,11 @@ var VueNavigationLink = Vue.extend({
       required: false,
       type: String,
       default: null
+    },
+    lone: {
+      required: false,
+      type: Boolean,
+      default: false
     }
   }
 });
