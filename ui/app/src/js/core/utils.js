9af62ab042fe614856b9c0e87a24d5d5b782e955
Sergio Sanchez <sanchezs@vmware.com>
2016-12-28 10:46:59 +0200

    Avoid showing "Terminal" button to STOPPED containers
    Hidden "Terminal" button for containers that are in STOPPED state.
    
    Change-Id: Ic96d138106ed56c9e9638827a2a3909cbb25012b
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/5161
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>
    Reviewed-by: Iveta Ilieva <iilieva@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>

--- ui/app/src/js/core/utils.js
+++ ui/app/src/js/core/utils.js
@@ -512,7 +512,8 @@ var utils = {
     } else if (op === constants.CONTAINERS.OPERATION.SHELL) {
       return this.getConfigurationPropertyBoolean('allow.browser.ssh.console')
                 && !this.isApplicationSingleView()
-                && this.isContainerStatusOk(resource.powerState);
+                && this.isContainerStatusOk(resource.powerState)
+                && !this.isContainerStatusInactive(resource.powerState);
     }
 
     return true;
