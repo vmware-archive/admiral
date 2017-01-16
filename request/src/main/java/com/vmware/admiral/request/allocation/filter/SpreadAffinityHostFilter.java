917a83be79ab6e1a06826cf6be71464c07b9cabe
Zahari Ivanov <zaharii@vmware.com>
2017-01-16 15:27:30 +0200

    Implementation of "Spread" advanced policy.
    Current change introduces new AffinityHost filter which is called SpreadAffinityHostFilter.
    Its purpose is to filter hosts by number of resources - Containers/Computes.
    This is first part of the filter - for Containers, second will come afterwards for Computes.
    
    Change-Id: Ia9d95515fbdf73374d13741f5248394d0563b303
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/5684
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Peter Mitrov <pmitrov@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>

---
+++ request/src/main/java/com/vmware/admiral/request/allocation/filter/SpreadAffinityHostFilter.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+package com.vmware.admiral.request.allocation.filter;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+import com.vmware.admiral.common.util.QueryUtil;
+import com.vmware.admiral.common.util.ServiceDocumentQuery;
+import com.vmware.admiral.compute.ElasticPlacementZoneConfigurationService;
+import com.vmware.admiral.compute.ElasticPlacementZoneConfigurationService.ElasticPlacementZoneConfigurationState;
+import com.vmware.admiral.compute.ElasticPlacementZoneService;
+import com.vmware.admiral.compute.container.ContainerDescriptionService.ContainerDescription;
+import com.vmware.admiral.compute.container.ContainerService.ContainerState;
+
+import com.vmware.admiral.request.PlacementHostSelectionTaskService;
+import com.vmware.admiral.request.PlacementHostSelectionTaskService.PlacementHostSelectionTaskState;
+import com.vmware.admiral.request.ReservationTaskFactoryService;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceHost;
+import com.vmware.xenon.common.UriUtils;
+import com.vmware.xenon.common.Utils;
+import com.vmware.xenon.services.common.QueryTask;
+
+
+/**
+*
+*  A filter implementing {@link HostSelectionFilter} aimed to provide host selection based on Placement - SPREAD.
+*
+*  Algorithm will filter out the docker hosts leaving only the host with smallest number of containers.
+*   1) Let h ∈ { h(1)....h(n-1), h(n) }
+*   2) P(h) = min{h(1)...h(n)}
+*   3) ∃! h: P(h)
+*
+*   Constraint (1) means that most suitable host belongs to set of hosts which have been filtered from other affinity filters.
+*   Constraint (2) means that hosts will be sorted by number of containers in ascending order.
+*   Constraint (3) means there is exactly one host such that P(h) is true => Host with smallest number of containers will be returned.
+*
+*/
+public class SpreadAffinityHostFilter implements
+        HostSelectionFilter<PlacementHostSelectionTaskService.PlacementHostSelectionTaskState> {
+
+    private final ServiceHost host;
+
+    public SpreadAffinityHostFilter(ServiceHost host, ContainerDescription desc) {
+        this.host = host;
+    }
+
+    @Override
+    public boolean isActive() {
+        return true;
+    }
+
+    @Override
+    public Map<String, AffinityConstraint> getAffinityConstraints() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public void filter(PlacementHostSelectionTaskState state,
+            Map<String, HostSelection> hostSelectionMap,
+            HostSelectionFilterCompletion callback) {
+
+        // Nothing to sort here.
+        if (hostSelectionMap.size() == 1) {
+            callback.complete(hostSelectionMap, null);
+            return;
+        }
+
+        String serviceLink = state.serviceTaskCallback != null
+                ? state.serviceTaskCallback.serviceSelfLink
+                : null;
+        // Filter should be ignored on Reservation stage.
+        if (serviceLink != null
+                && serviceLink.startsWith(ReservationTaskFactoryService.SELF_LINK)) {
+            callback.complete(hostSelectionMap, null);
+            return;
+        }
+
+        // On allocation stage only one resource pool exists.
+        String resourcePoolLink = state.resourcePoolLinks.get(0);
+
+        URI uri = UriUtils.buildUri(host, String.format("%s/%s",
+                ElasticPlacementZoneConfigurationService.SELF_LINK, resourcePoolLink));
+
+        host.sendRequest(Operation.createGet(uri)
+                .setReferer(host.getUri())
+                .setCompletion((o, ex) -> {
+
+                    if (ex != null) {
+                        host.log(Level.WARNING, Utils.toString(ex));
+                        callback.complete(hostSelectionMap, null);
+                        return;
+                    }
+
+                    ElasticPlacementZoneConfigurationState epz = o
+                            .getBody(ElasticPlacementZoneConfigurationState.class);
+                    if (epz != null && epz.epzState != null
+                            && epz.epzState.placementPolicy == ElasticPlacementZoneService.PlacementPolicy.SPREAD) {
+                        // First find how many resources every host has.
+                        retrieveContainers(hostSelectionMap, callback);
+                    } else {
+                        callback.complete(hostSelectionMap, null);
+                    }
+                }));
+
+    }
+
+    private void retrieveContainers(
+            Map<String, HostSelection> hostSelectionMap,
+            HostSelectionFilterCompletion callback) {
+
+        QueryTask.Query.Builder queryBuilder = QueryTask.Query.Builder.create()
+                .addKindFieldClause(ContainerState.class)
+                .addInClause(ContainerState.FIELD_NAME_PARENT_LINK, hostSelectionMap.keySet());
+
+        QueryTask q = QueryTask.Builder.create().setQuery(queryBuilder.build()).build();
+        q.querySpec.resultLimit = ServiceDocumentQuery.DEFAULT_QUERY_RESULT_LIMIT;
+        QueryUtil.addExpandOption(q);
+
+        new ServiceDocumentQuery<>(host, ContainerState.class).query(q, (r) -> {
+            if (r.hasException()) {
+                host.log(Level.WARNING,
+                        "Exception while quering containers during 'Spread' filtering."
+                                + "Error: [%s]",
+                        r.getException().getMessage());
+                callback.complete(null, r.getException());
+                return;
+            } else if (r.hasResult()) {
+                HostSelection hostSelection = hostSelectionMap
+                        .get(r.getResult().parentLink);
+                hostSelection.resourceCount += 1;
+            } else {
+                // Return the host with minimum number of containers.
+                completeFilter(hostSelectionMap, callback);
+            }
+        });
+    }
+
+    private void completeFilter(Map<String, HostSelection> hostSelectionMap,
+            HostSelectionFilterCompletion callback) {
+
+        // Sort hosts by number of resources.
+        List<HostSelection> hostSelections = new ArrayList<>(hostSelectionMap.values());
+        // i.e. hosts [A(5), B(3), C(9)] -> [B(3), A(5), C(9)]
+        hostSelections.sort(Comparator.comparing(h -> h.resourceCount));
+
+        // Host with smallest number of containers is the first element.
+        HostSelection mostLoadedHost = hostSelections.get(0);
+
+        Map<String, HostSelection> result = new HashMap<>();
+        result.put(mostLoadedHost.hostLink, mostLoadedHost);
+
+        callback.complete(result, null);
+    }
+
+}
---
+++ request/src/test/java/com/vmware/admiral/request/allocation/filter/SpreadAffinityHostFilterTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+package com.vmware.admiral.request.allocation.filter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import com.vmware.admiral.compute.ElasticPlacementZoneConfigurationService;
+import com.vmware.admiral.compute.ElasticPlacementZoneConfigurationService.ElasticPlacementZoneConfigurationState;
+import com.vmware.admiral.compute.ElasticPlacementZoneService;
+import com.vmware.admiral.compute.ElasticPlacementZoneService.ElasticPlacementZoneState;
+import com.vmware.admiral.compute.container.ContainerDescriptionService;
+import com.vmware.admiral.compute.container.ContainerDescriptionService.ContainerDescription;
+import com.vmware.admiral.request.allocation.filter.HostSelectionFilter.HostSelection;
+import com.vmware.admiral.request.util.TestRequestStateFactory;
+import com.vmware.xenon.common.Service.Action;
+import com.vmware.xenon.common.UriUtils;
+
+
+public class SpreadAffinityHostFilterTest extends BaseAffinityHostFilterTest {
+
+    @Test
+    public void testFilterDoesNotAffectHosts() throws Throwable {
+        filter = new SpreadAffinityHostFilter(host, containerDesc);
+        assertTrue(filter.isActive());
+        Map<String, HostSelection> selected = filter();
+        assertEquals(3, selected.size());
+    }
+
+    @Test
+    public void testSpreadFilter() throws Throwable {
+
+        String firstHost = initialHostLinks.get(0);
+        String secondHost = initialHostLinks.get(1);
+        String thirdHost = initialHostLinks.get(2);
+
+        assignContainersToHost(firstHost, 5);
+        assignContainersToHost(secondHost, 9);
+        assignContainersToHost(thirdHost, 3);
+
+        // Set Spread policy to EPZS
+        uodateEpzWithPlacementPolicy();
+
+        // Filter should return thirdHost as it has lowest amount of containers.
+        filter = new SpreadAffinityHostFilter(host, containerDesc);
+
+        Map<String, HostSelection> selected = filter();
+
+        assertTrue(!selected.isEmpty());
+
+        assertEquals(1, selected.size());
+
+        assertTrue(selected.containsKey(thirdHost));
+
+        // Change number of containers per host.
+        assignContainersToHost(firstHost, 15);
+        // secondHost won't be updated. It will contain smallest amount of containers.
+        assignContainersToHost(thirdHost, 23);
+
+        // Clear previous result.
+        selected.clear();
+
+        // secontHost should be the result.
+        selected = filter();
+
+        assertTrue(!selected.isEmpty());
+
+        assertEquals(1, selected.size());
+
+        assertTrue(selected.containsKey(secondHost));
+
+    }
+
+    private void uodateEpzWithPlacementPolicy() throws Throwable {
+
+        // Create ElasticPlacementZoneState which follows SPREAD deployment policy.
+        ElasticPlacementZoneState epzState = new ElasticPlacementZoneState();
+        epzState.placementPolicy = ElasticPlacementZoneService.PlacementPolicy.SPREAD;
+        epzState.resourcePoolLink = resourcePool.documentSelfLink;
+
+        ElasticPlacementZoneConfigurationState epz = new ElasticPlacementZoneConfigurationState();
+        epz.documentSelfLink = resourcePool.documentSelfLink;
+        epz.resourcePoolState = resourcePool;
+        epz.epzState = epzState;
+
+        epz = doOperation(epz,
+                UriUtils.buildUri(host, ElasticPlacementZoneConfigurationService.SELF_LINK),
+                ElasticPlacementZoneConfigurationState.class, false, Action.PATCH);
+
+        assertEquals(epz.epzState.placementPolicy,
+                ElasticPlacementZoneService.PlacementPolicy.SPREAD);
+
+    }
+
+    private void assignContainersToHost(String hostLink, int instances) throws Throwable {
+        for (int i = 0; i <= instances; i++) {
+            ContainerDescription desc = TestRequestStateFactory.createContainerDescription();
+            desc.documentSelfLink = UUID.randomUUID().toString();
+            containerDesc = doPost(desc, ContainerDescriptionService.FACTORY_LINK);
+            createContainerWithDifferentContextId(desc, hostLink);
+        }
+    }
+
+}
