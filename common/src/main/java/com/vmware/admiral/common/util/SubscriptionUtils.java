ae9637ebe555ce1469982e54656a06f6f4ae71cb
Tony Georgiev <tgeorgiev@vmware.com>
2017-02-01 16:21:11 +0200

    Code refactoring
    Remove some unused code
    Cover, untested utility methods
    
    Change-Id: I7ab295b91b230b55c41f1274b7c2981ffec22f5e
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/6321
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>

--- common/src/main/java/com/vmware/admiral/common/util/SubscriptionUtils.java
+++ common/src/main/java/com/vmware/admiral/common/util/SubscriptionUtils.java
@@ -11,17 +11,14 @@
 
 package com.vmware.admiral.common.util;
 
-import java.util.Set;
 import java.util.function.Consumer;
 
 import com.vmware.xenon.common.Operation;
-import com.vmware.xenon.common.Service.Action;
 import com.vmware.xenon.common.ServiceDocument;
 import com.vmware.xenon.common.ServiceSubscriptionState.ServiceSubscriber;
 import com.vmware.xenon.common.StatefulService;
 import com.vmware.xenon.common.TaskState;
 import com.vmware.xenon.common.TaskState.TaskStage;
-import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.common.Utils;
 import com.vmware.xenon.services.common.ReliableSubscriptionService;
 
@@ -46,93 +43,6 @@ public class SubscriptionUtils {
                 subscribeOp, subscribeBody, notificationConsumer);
         service.getHost().startSubscriptionService(subscribeOp,
                 notificationTarget, subscribeBody);
-    }
-
-    /**
-     * handle subscriptions from multiple services
-     * 1. Mark operation as complete
-     * 2. If the operation is not PUT or POST, return
-     * 3. If the task has failed, send failure patch to service
-     * 4. Update the list of services from which we have received notification
-     * 5. Unsubscribe from notifications, optionally delete the task that raised the notification
-     * 6. If we have received the expected number of notifications, patch the next state back
-     *
-     * @param service Stateful provisioning service
-     * @param update Notification operation
-     * @param notificationTaskLink Self link of the task that raised the notification
-     * @param opTaskState TaskState of the service that raised the notification
-     * @param expectedNotificationCount Expected number of notifications
-     * @param returnState The next state for the StatefulService
-     * @param finishedTaskLinks Set of self links for services from which we have received notification
-     * @param deleteTask flag to delete the service that raised the notification
-     */
-    public static void handleSubscriptionNotifications(StatefulService service, Operation update,
-            String notificationTaskLink, TaskState opTaskState,
-            int expectedNotificationCount, Object returnState, Set<String> finishedTaskLinks,
-            boolean deleteTask) {
-        int finishedTaskCount;
-
-        update.complete();
-
-        if ((update.getAction() != Action.PATCH && update.getAction() != Action.PUT)) {
-            return;
-        }
-
-        // Fail if task was cancelled or has failed
-        if (TaskState.isCancelled(opTaskState)
-                || TaskState.isFailed(opTaskState)) {
-            SubscriptionUtils.sendFailureSelfPatch(service,
-                    new IllegalStateException("Operation failed:"
-                            + Utils.toJsonHtml(update)));
-            if (deleteTask) {
-                service.sendRequest(Operation
-                        .createDelete(
-                                service, notificationTaskLink)
-                        .setBody(new ServiceDocument()));
-            }
-            return;
-        }
-
-        // Ignore if task has not finished yet
-        if (!TaskState.isFinished(opTaskState)) {
-            return;
-        }
-
-        // Ignore if task has already been seen
-        synchronized (finishedTaskLinks) {
-            if (!finishedTaskLinks.add(notificationTaskLink)) {
-                return;
-            }
-
-            // Retrieve size in synchronized block to prevent racing
-            finishedTaskCount = finishedTaskLinks.size();
-        }
-
-        Operation deleteOp = Operation.createDelete(service, notificationTaskLink)
-                .setReferer(service.getUri())
-                .setCompletion(
-                        (delOp, delEx) -> {
-                            if (delEx != null) {
-                                service.logWarning("Stopping subscriber failed %s",
-                                        Utils.toString(delEx));
-                                return;
-                            }
-                        });
-        service.getHost().stopSubscriptionService(deleteOp,
-                UriUtils.buildPublicUri(service.getHost(), update.getUri().getPath()));
-
-        if (deleteTask) {
-            service.sendRequest(Operation
-                    .createDelete(
-                            service, notificationTaskLink)
-                    .setBody(new ServiceDocument()));
-        }
-
-        //patch service with return state when we have seen all tasks finish
-        if (finishedTaskCount == expectedNotificationCount) {
-            // Patch back the return state
-            SubscriptionUtils.sendPatch(service, returnState);
-        }
     }
 
     /**
