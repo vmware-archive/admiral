1427fb2034b08474d021ef05da720e85ec1925b9
Tony Georgiev <tgeorgiev@vmware.com>
2017-03-06 16:18:45 +0200

    Add common grid and card components
    Change-Id: Ia55e0c7d7e4ee4677ce301b747c9069936340231
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/7606
    Compute-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Rostislav Hristov <rhristov@vmware.com>

---
+++ ui/ng-app/src/app/components/card/card.component.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  imageSrc: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
