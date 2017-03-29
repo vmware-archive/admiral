76fb911e12059af277902f4912f6a3ec473f5257
Tony Georgiev <tgeorgiev@vmware.com>
2017-03-29 19:56:14 +0300

    Allow to pass only overridable properties as part of the container configuration
    Instead of porviding all distributed properties + the ones that needs override
    provide only the different ones.
    
    Change-Id: Ib5ea232cfac88a1e20216f4c2e7a113471d06716
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/8490
    Reviewed-by: Grigor Lechev <glechev@vmware.com>
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>

--- host/entrypoint.sh
+++ host/entrypoint.sh
@@ -13,9 +13,16 @@ then
 MEMORY_OPTS="-Xmx768M -Xms768M -Xss256K -Xmn256M -XX:MaxMetaspaceSize=256m"
 fi
 
+CONFIG_FILES="$DIST_CONFIG_FILE_PATH"
+if [ -f $CONFIG_FILE_PATH ]
+then
+CONFIG_FILES="$CONFIG_FILES,$CONFIG_FILE_PATH"
+fi
+
+
 JAVA_OPTS="$JAVA_OPTS $MEMORY_OPTS"
 
-java -Dconfiguration.properties=$CONFIG_FILE_PATH $JAVA_OPTS -cp $ADMIRAL_ROOT/*:$ADMIRAL_ROOT/lib/*:/etc/xenon/dynamic-services/* com.vmware.admiral.host.ManagementHost --bindAddress=0.0.0.0 --port=$ADMIRAL_PORT --sandbox=$ADMIRAL_STORAGE_PATH $XENON_OPTS &
+java -Dconfiguration.properties=$CONFIG_FILES $JAVA_OPTS -cp $ADMIRAL_ROOT/*:$ADMIRAL_ROOT/lib/*:/etc/xenon/dynamic-services/* com.vmware.admiral.host.ManagementHost --bindAddress=0.0.0.0 --port=$ADMIRAL_PORT --sandbox=$ADMIRAL_STORAGE_PATH $XENON_OPTS &
 PID=$!
 
 wait $PID
