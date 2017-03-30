86efc7ecabed3af5f34c97b058e477751f51a373
Angel Ivanov <angeli@vmware.com>
2017-03-30 11:25:51 +0300

    Enable cluster tests with 2 nodes.
    Change-Id: I61e40896e69c825622b9f99cd0d2958742b1d50c
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/8506
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Zahari Ivanov <zaharii@vmware.com>

--- test-integration/src/test/java/com/vmware/admiral/host/ManagementHostClusterOf2NodesIT.java
+++ test-integration/src/test/java/com/vmware/admiral/host/ManagementHostClusterOf2NodesIT.java
@@ -13,30 +13,39 @@ package com.vmware.admiral.host;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import static com.vmware.admiral.host.ManagementHostAuthUsersTest.AUTH_TOKEN_RETRY_COUNT;
+import static com.vmware.admiral.host.ManagementHostAuthUsersTest.doRestrictedOperation;
 import static com.vmware.admiral.host.ManagementHostAuthUsersTest.login;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.vmware.admiral.common.DeploymentProfileConfig;
 import com.vmware.admiral.compute.ResourceType;
 import com.vmware.admiral.compute.container.CompositeDescriptionService.CompositeDescription;
 import com.vmware.admiral.compute.container.ContainerDescriptionService;
 import com.vmware.admiral.compute.container.ContainerDescriptionService.ContainerDescription;
-import com.vmware.admiral.compute.container.ContainerService.ContainerState.PowerState;
 import com.vmware.admiral.request.RequestBrokerFactoryService;
 import com.vmware.admiral.request.RequestBrokerService.RequestBrokerState;
 import com.vmware.admiral.request.util.TestRequestStateFactory;
@@ -49,135 +58,82 @@ import com.vmware.xenon.common.test.TestContext;
  * Similar to {@link ManagementHostAuthUsersIT} but this test includes 2 nodes, only SSL enabled and
  * the users' passwords are encrypted.
  */
-@Ignore("VBV-1018")
 public class ManagementHostClusterOf2NodesIT extends BaseManagementHostClusterIT {
 
     private List<ManagementHost> hostsToTeardown;
 
+    private int portOne;
+    private int portTwo;
+
+    private String hostOneAddress;
+    private String hostTwoAddress;
+    private List<String> allHosts;
+
     private ManagementHost hostOne;
     private ManagementHost hostTwo;
 
-    private int PORT_ONE;
-    private int PORT_TWO;
-
-    private String HOST_ONE;
-    private String HOST_TWO;
-    private List<String> ALL_HOSTS;
-
     @Before
-    public void setUp() throws Throwable {
-        PORT_ONE = 20000 + new Random().nextInt(5000);
-        PORT_TWO = PORT_ONE + 1;
-
-        HOST_ONE = LOCALHOST + PORT_ONE;
-        HOST_TWO = LOCALHOST + PORT_TWO;
-
-        ALL_HOSTS = Arrays.asList(HOST_ONE, HOST_TWO);
-
-        hostOne = setUpHost(PORT_ONE, null, Arrays.asList(HOST_ONE));
-        waitAuthServices(hostOne);
-
-        hostTwo = setUpHost(PORT_TWO, null, ALL_HOSTS);
-        waitAuthServices(hostTwo);
-        String tokenOne = login(hostOne, USERNAME, PASSWORD, true);
-        TestContext ctx = new TestContext(1, Duration.ofSeconds(60));
-        createUpdateQuorumOperation(hostOne, 2, ctx, tokenOne, AUTH_TOKEN_RETRY_COUNT);
-        ctx.await();
-
-        hostsToTeardown = Arrays.asList(hostOne, hostTwo);
-
-        // Initialize provisioning context
-        initializeProvisioningContext(hostOne);
+    public void setUp() {
+        DeploymentProfileConfig.getInstance().setTest(true);
+        setupHostWithRetry(3);
     }
 
     @After
-    public void tearDown() throws Throwable {
+    public void tearDown() {
         tearDownHost(hostsToTeardown);
+    }
+
+    @BeforeClass
+    public static void init() {
+        scheduler = Executors.newScheduledThreadPool(1);
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        scheduler.shutdown();
     }
 
     @Test
     public void testRestrictedOperationWithNodesRestarted() throws Throwable {
+        // Test login with invalid credentials.
+        logger.log(Level.INFO, "asserting login with invalid credentials");
+        assertNull(login(hostOne, USERNAME, "bad"));
+        assertNull(login(hostOne, "bad", PASSWORD));
 
-        /*
-         * Within this test the nodes are restarted but not at the same time, there's always a
-         * running node.
-         */
+        assertNull(login(hostTwo, USERNAME, "bad"));
+        assertNull(login(hostTwo, "bad", PASSWORD));
 
+        // Restricted operation without authentication.
+        assertEquals(HttpURLConnection.HTTP_FORBIDDEN, doRestrictedOperation(hostOne, null));
+        assertEquals(HttpURLConnection.HTTP_FORBIDDEN, doRestrictedOperation(hostTwo, null));
+
+        // Update quorum of host two to 1, because ClusterMonitoringService is disabled.
+        String tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
+        TestContext ctx = new TestContext(1, Duration.ofMinutes(1));
+        createUpdateQuorumOperation(hostTwo, 1, ctx, tokenTwo, AUTH_TOKEN_RETRY_COUNT);
+        ctx.await();
+
+        // Stop node 1 and wait until it's removed from the node group.
+        logger.log(Level.INFO, "stopping host one");
+        stopHostAndRemoveItFromNodeGroup(hostTwo, hostOne);
+
+        logger.log(Level.INFO, "asserting cluster with host two");
+        tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
+        assertClusterWithToken(tokenTwo, hostTwo);
+
+        logger.log(Level.INFO, "starting host one and asserting cluster");
+        logger.log(Level.INFO, "All hosts are " + allHosts);
+        hostOne = startHost(hostOne, null, allHosts, 3);
         String tokenOne = login(hostOne, USERNAME, PASSWORD, true);
         assertClusterWithToken(tokenOne, hostOne);
 
-        String tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenTwo, hostTwo);
-
         List<ManagementHost> hosts = Arrays.asList(hostOne, hostTwo);
-        validateDefaultContentAdded(hosts);
-
-        /*
-         * ==== Restart node1 ====================================================================
-         */
-
-        stopHost(hostOne, false);
-
-        // We should explicitly set the quorum in the running node to 1 here, but now the
-        // ClusterMonitoringService will take care of that. Otherwise the next operation will hang.
-
-        assertClusterWithToken(tokenTwo, hostTwo);
-        assertClusterFromNodes(hostTwo);
-
-        hostOne = startHost(hostOne, null, ALL_HOSTS);
-
-        tokenOne = login(hostOne, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenOne, hostOne);
-
-        tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenTwo, hostTwo);
-        assertClusterFromNodes(hostOne, hostTwo);
-
-        hosts = Arrays.asList(hostOne, hostTwo);
-        validateDefaultContentAdded(hosts);
-
-        /*
-         * ==== Restart node2 ====================================================================
-         */
-
-        stopHost(hostTwo, false);
-
-        // We should explicitly set the quorum in the running node to 1 here, but now the
-        // ClusterMonitoringService will take care of that. Otherwise the next operation will hang.
-
-        tokenOne = login(hostOne, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenOne, hostOne);
-        assertClusterFromNodes(hostOne);
-
-        hostTwo = startHost(hostTwo, null, ALL_HOSTS);
-
-        tokenOne = login(hostOne, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenOne, hostOne);
-
-        tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenTwo, hostTwo);
-        assertClusterFromNodes(hostOne, hostTwo);
-
-        hosts = Arrays.asList(hostOne, hostTwo);
         validateDefaultContentAdded(hosts);
     }
 
+    @Ignore("To be further reworked or removed.")
     @Test
     public void testRestrictedOperationWithNodesStoppedAndStarted() throws Throwable {
-
-        /*
-         * Within this test the nodes are restarted at the same time, both nodes are stopped and
-         * then started again.
-         */
-
-        String tokenOne = login(hostOne, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenOne, hostOne);
-
-        String tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
-        assertClusterWithToken(tokenTwo, hostTwo);
-
-        List<ManagementHost> hosts = Arrays.asList(hostOne, hostTwo);
-        validateDefaultContentAdded(hosts);
 
         /*
          * ==== Stop both nodes ==================================================================
@@ -191,34 +147,38 @@ public class ManagementHostClusterOf2NodesIT extends BaseManagementHostClusterIT
          */
 
         // start 1st node with quorum = 1 (peer nodes = self)
-        hostOne = startHost(hostOne, hostOne.getStorageSandbox(), Arrays.asList(HOST_ONE));
+        hostOne = startHost(hostOne, hostOne.getStorageSandbox(), Arrays.asList(hostOneAddress));
         // start 2nd node with quorum = 2 (peer nodes = self & 1st node )
-        hostTwo = startHost(hostTwo, null, ALL_HOSTS);
+        hostTwo = startHost(hostTwo, null, allHosts);
 
-        tokenOne = login(hostOne, USERNAME, PASSWORD, true);
+        String tokenOne = login(hostOne, USERNAME, PASSWORD, true);
         assertClusterWithToken(tokenOne, hostOne);
 
-        tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
+        String tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
         assertClusterWithToken(tokenTwo, hostOne, hostTwo);
 
         assertClusterFromNodes(hostOne, hostTwo);
 
-        hosts = Arrays.asList(hostOne, hostTwo);
+        List<ManagementHost> hosts = Arrays.asList(hostOne, hostTwo);
         validateDefaultContentAdded(hosts);
     }
 
     @Test
     public void testReplicationOfDocumentsAfterRestart() throws Throwable {
-
         Map<String, String> headers = new HashMap<>();
         headers.put(Operation.REQUEST_AUTH_TOKEN_HEADER, login(hostOne, USERNAME, PASSWORD, true));
 
         assertContainerDescription(hostOne, headers);
         assertContainerDescription(hostTwo, headers);
 
-        // Restart hostTwo
-        stopHost(hostTwo, false);
-        hostTwo = startHost(hostTwo, null, ALL_HOSTS);
+        // Update quorum of host two to 1, because ClusterMonitoringService is disabled.
+        String tokenTwo = login(hostTwo, USERNAME, PASSWORD, true);
+        TestContext ctx = new TestContext(1, Duration.ofMinutes(1));
+        createUpdateQuorumOperation(hostTwo, 1, ctx, tokenTwo, AUTH_TOKEN_RETRY_COUNT);
+        ctx.await();
+
+        stopHostAndRemoveItFromNodeGroup(hostOne, hostTwo);
+        hostTwo = startHost(hostTwo, null, allHosts, 3);
 
         // Get token from hostOne
         headers = new HashMap<>();
@@ -278,8 +238,6 @@ public class ManagementHostClusterOf2NodesIT extends BaseManagementHostClusterIT
         assertNotNull(container);
         assertEquals(ContainerDescriptionService.FACTORY_LINK + "/test-container-desc",
                 container.descriptionLink);
-        assertEquals(PowerState.RUNNING.toString(), container.powerState);
-
     }
 
     @Test
@@ -338,6 +296,46 @@ public class ManagementHostClusterOf2NodesIT extends BaseManagementHostClusterIT
         assertNotNull(compositeComponent.componentLinks);
         assertEquals(2, compositeComponent.componentLinks.size());
 
+    }
+
+    private void setupHostWithRetry(int retryCount) {
+        try {
+            // Get ports
+            portOne = 20000 + new Random().nextInt(5000);
+            portTwo = portOne + 1;
+
+            // Build addresses
+            hostOneAddress = LOCALHOST + portOne;
+            hostTwoAddress = LOCALHOST + portTwo;
+            allHosts = Collections.synchronizedList(
+                    Arrays.asList(hostOneAddress, hostTwoAddress));
+
+            hostsToTeardown = new ArrayList<>();
+
+            // Setup first host.
+            logger.log(Level.INFO, "setting up first host");
+            hostOne = setUpHost(portOne, null, Arrays.asList(hostOneAddress));
+            hostsToTeardown.add(hostOne);
+
+            // Setup second host.
+            logger.log(Level.INFO, "setting up second host");
+            hostTwo = setUpHost(portTwo, null, Arrays.asList(hostOneAddress, hostTwoAddress));
+            String tokenOne = login(hostOne, USERNAME, PASSWORD, true);
+            TestContext ctx = new TestContext(1, Duration.ofMinutes(1));
+            createUpdateQuorumOperation(hostOne, 2, ctx, tokenOne, AUTH_TOKEN_RETRY_COUNT);
+            ctx.await();
+            hostsToTeardown.add(hostTwo);
+
+            initializeProvisioningContext(hostOne);
+        } catch (Throwable ex) {
+            logger.log(Level.SEVERE, "Setting up hosts failed: " + Utils.toString(ex));
+            if (retryCount > 0) {
+                tearDownHost(hostsToTeardown);
+                setupHostWithRetry(retryCount - 1);
+            } else {
+                fail(Utils.toString(ex));
+            }
+        }
     }
 
 }
