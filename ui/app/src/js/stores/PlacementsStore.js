fc0e1f8eb7e91d7b0ed5f159b55dc7d4aa9a824a
Stanislav Hadjiiski <hadjiiskis@vmware.com>
2017-04-05 15:02:23 +0300

    Fix auto-generated placement zone and placement for VCH in vRA mode
    Until now, the placement zone and placement were successfully
    auto-generated, but were not displayed in the placements and placements
    zones views, since they were lacking the appropripriate tenantLinks. Now,
    the tenantLinks from the added VCH hosts are propagated to the placement
    zone and placement.
    
    Change-Id: I545ab7424f4743b9735009f75051130ba7809a3f
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/8723
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>
    Compute-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>

--- ui/app/src/js/stores/PlacementsStore.js
+++ ui/app/src/js/stores/PlacementsStore.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2017 VMware, Inc. All Rights Reserved.
  *
  * This product is licensed to you under the Apache License, Version 2.0 (the "License").
  * You may not use this product except in compliance with the License.
@@ -312,7 +312,8 @@ let PlacementsStore = Reflux.createStore({
     if (!utils.isApplicationEmbedded()) {
       let resourceGroupLinks = placements
                                     .filter((placement) => placement.tenantLinks)
-                                    .map((placement) => utils.getGroup(placement.tenantLinks));
+                                    .map((placement) => utils.getGroup(placement.tenantLinks))
+                                    .filter((groupId) => groupId);
 
       calls.push(services.loadResourceGroups([...new Set(resourceGroupLinks)]).then((result) => {
         this.setInData(['placements', 'groups'], Object.values(result));
