0b81717772d35f36f88b230ce5cb003dac4220b0
sibing <sibing@vmware.com>
2017-04-13 10:43:13 +0300

    Expose Machine Details view.
    The idea is to consume it from vRA Catalog.
    
    https://jira-hzn.eng.vmware.com/browse/VCOM-545
    
    Testing Done: Locally - local admiral and local cafe-container.
    
    Change-Id: I398a8f49edbb82beeca788f3ee154e90c5230c68
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/9161
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Rostislav Hristov <rhristov@vmware.com>

--- ui/app/src/js/components/SingleView.js
+++ ui/app/src/js/components/SingleView.js
@@ -14,6 +14,8 @@ import RequestGraph from 'components/requests/RequestGraph';
 import ContainerDefinitionForm from 'components/containers/ContainerDefinitionForm';
 import ContainersStore from 'stores/ContainersStore';
 import RequestGraphStore from 'stores/RequestGraphStore';
+import MachinesStore from 'stores/MachinesStore';
+import MachineDetails from 'components/machines/MachineDetailsView';
 import * as actions from 'actions/Actions';
 import modal from 'core/modal';
 
@@ -37,6 +39,12 @@ function removeOldViews() {
 
 function openContainerDetails() {
   this.view = new ContainerDetails();
+  this.$el.append(this.view.getEl());
+  this.view.attached();
+}
+
+function openMachineDetails() {
+  this.view = new MachineDetails();
   this.$el.append(this.view.getEl());
   this.view.attached();
 }
@@ -77,6 +85,12 @@ function addEventListeners() {
       _this.view.setData(data);
     }
   });
+
+  MachinesStore.listen(function(data) {
+    if (_this.view instanceof MachineDetails && data.selectedItemDetails) {
+      _this.view.setData(data.selectedItemDetails);
+    }
+  });
 }
 
 function initRoutes() {
@@ -91,6 +105,11 @@ function initRoutes() {
   crossroads.addRoute('/containers/{containerId}', function(containerId) {
     openContainerDetails.call(_this);
     actions.ContainerActions.openContainerDetails(containerId);
+  });
+
+  crossroads.addRoute('/machines/{machineId}/details', function(machineId) {
+    openMachineDetails.call(_this);
+    actions.MachineActions.openMachineDetails(machineId);
   });
 
   crossroads.addRoute('/request-graph/{requestId}:?host:', function(requestId, query) {
