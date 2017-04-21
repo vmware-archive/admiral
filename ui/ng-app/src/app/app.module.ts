01a853fdba15b9b139f651ec58ad08406b0781cf
Tony Georgiev <tgeorgiev@vmware.com>
2017-04-22 02:20:16 +0300

    add infinite scroll
    Change-Id: Ic8f85e3e9393082ed129f839249dae2e7be48863

--- ui/ng-app/src/app/app.module.ts
+++ ui/ng-app/src/app/app.module.ts
@@ -10,7 +10,7 @@
  */
 
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule, FormsModule }          from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
