78c257dd4d2b7d4ea6480d36a569eee68fd31094
aangelov <aangelov@vmware.com>
2017-05-10 11:40:22 +0300

    On init make services to wait common services to become available.
    (cherry picked from commit dfbd43a1c3886c6adce6a46515f892a011655c45)
    
    Change-Id: I196ee7fb13ecc9ab5b5f6afbbf6c1315f6d36ed2
    
    (cherry picked from commit 2864c8f)

--- common/src/main/java/com/vmware/admiral/host/HostInitCommonServiceConfig.java
+++ common/src/main/java/com/vmware/admiral/host/HostInitCommonServiceConfig.java
@@ -11,6 +11,11 @@
 
 package com.vmware.admiral.host;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+
 import com.vmware.admiral.log.EventLogService;
 import com.vmware.admiral.service.common.ClusterMonitoringService;
 import com.vmware.admiral.service.common.CommonInitialBootService;
@@ -33,6 +38,8 @@ import com.vmware.xenon.common.ServiceHost;
 import com.vmware.xenon.common.UriUtils;
 
 public class HostInitCommonServiceConfig extends HostInitServiceHelper {
+    public static final int COMMON_SERVICES_AVAILABILITY_TIMEOUT = Integer
+            .getInteger("common.services.initialization.timeout.seconds", 30);
 
     public static void startServices(ServiceHost host) {
         startServices(host, NodeMigrationService.class, NodeHealthCheckService.class,
@@ -52,11 +59,41 @@ public class HostInitCommonServiceConfig extends HostInitServiceHelper {
 
         // start initialization of system documents, posting with pragma to queue a request,
         // for a service to become available
+        Throwable[] t = new Throwable[1];
+        CountDownLatch l = new CountDownLatch(1);
         host.sendRequest(Operation
                 .createPost(UriUtils.buildUri(host, CommonInitialBootService.class))
                 .addPragmaDirective(Operation.PRAGMA_DIRECTIVE_QUEUE_FOR_SERVICE_AVAILABILITY)
                 .setReferer(host.getUri())
-                .setBody(new ServiceDocument()));
+                .setBody(new ServiceDocument())
+                .setCompletion((o, e) -> {
+                    try {
+                        if (e != null) {
+                            host.log(Level.SEVERE, "Failute while waiting for service availability of common services: %s", e);
+                            t[0] = e;
+                            return;
+                        }
+                        host.log(Level.FINE, "Waiting for service availability of common services finished.");
+                    } finally {
+                        l.countDown();
+                    }
+                }
+        ));
+        try {
+            if (!l.await(COMMON_SERVICES_AVAILABILITY_TIMEOUT, TimeUnit.SECONDS)) {
+                t[0] = new TimeoutException("Waiting for service availability of common services timed out.");
+                host.log(Level.SEVERE, "Waiting for service availability of common services timed out: %s",
+                        t[0]);
+
+            }
+        } catch (InterruptedException e1) {
+            host.log(Level.SEVERE, "Waiting for service availability of common services was interupted: %s",
+                    e1);
+            t[0] = e1;
+        }
+        if (t[0] != null) {
+            throw new RuntimeException(t[0]);
+        }
     }
 
 }
