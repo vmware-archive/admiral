044318e196070b136dc97f64bc4b5765969c540b
Tony Georgiev <tgeorgiev@vmware.com>
2017-06-03 00:23:29 +0300

    Implement registries view
    Add old registry, new replication endpoints as well as replication rules
    
    Change-Id: Id32dd36436c7a285321faa508d60beb411406276
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/11057
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>

--- ui/ng-app/src/app/views/registries/registries.component.ts
+++ ui/ng-app/src/app/views/registries/registries.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-registries',
   templateUrl: './registries.component.html',
-  styleUrls: ['./registries.component.scss']
+  styleUrls: ['./registries.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegistriesComponent implements OnInit {
 
