b346f1f268d1b395615474b4b955c0c6cfcc1618
Tony Georgiev <tgeorgiev@vmware.com>
2017-06-02 22:57:19 +0300

    nit: enhance repositories view
    Change-Id: I801d84d9655f30d1af63da12416cd5229cadbe0e
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/11050
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Tony Georgiev <tgeorgiev@vmware.com>

--- ui/ng-app/src/app/views/hbr/repository/repository.component.ts
+++ ui/ng-app/src/app/views/hbr/repository/repository.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<hbr-repository [projectId]="projectId" [sessionInfo]="sessionInfo" style="width: 100%"></hbr-repository>'
+  template: `
+    <div class="main-view">
+      <div class="title">Project Repositories</div>
+      <hbr-repository [projectId]="projectId" [sessionInfo]="sessionInfo" style="margin-top: -50px; display: block;"></hbr-repository>
+    </div>
+  `
 })
 export class RepositoryComponent {
   projectId = 1;
