9a891ea4b5e08e395288ef8c5e35831d7c8c21e7
Tony Georgiev <tgeorgiev@vmware.com>
2017-06-12 16:11:16 +0300

    ui ng dev setup config improvements
    Change-Id: Idbe6504435e525035487c69148e1202e28718c65

--- ui/ng-app/proxy.conf.js
+++ ui/ng-app/proxy.conf.js
@@ -1,10 +1,55 @@
+const ENDPOINTS = [
+  '/tenants/*',
+  '/authn/*',
+  '/mgmt/*',
+  '/query/*',
+  '/core/*',
+  '/provisioning/*',
+  '/resources/*',
+  '/container-image-icons/*',
+  '/projects/*',
+  '/config/*',
+  '/templates/*',
+  '/groups/*',
+  '/image-assets/*',
+  '/user-session/*',
+  '/popular-images/*',
+  '/hbr-api/*',
+  '/auth/*',
+  '/index-no-navigation.html*',
+  '/lib/*',
+  '/js/*',
+  '/styles/*',
+  '/messages/*',
+  '/fonts/*',
+  '/util/*'
+];
 
-let configure = require('./base-proxy.conf.js');
+var configure = function(env) {
+  var configs = {};
+
+  ENDPOINTS.forEach((e) => {
+    configs[e] = {
+      'target': `http://${env.services.ip}:${env.services.port}`,
+      'secure': false
+    };
+  });
+
+  configs['/assets/i18n/base*.json'] = {
+    'target': `http://${env.services.ip}:${env.services.port}/ng`,
+    'secure': false
+  }
+
+  return configs;
+}
+
+var port = process.env.NG_PORT || '8282';
+console.log('proxying to localhost:' + port);
 
 var configs = configure({
   services: {
     ip: 'localhost',
-    port: '8282'
+    port: port
   }
 });
 
