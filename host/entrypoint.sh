0542d2369aa461445acc926bf4b27b42e804cc73
Lazarin Lazarov <llazarov@vmware.com>
2017-06-14 16:53:09 +0300

    Add retention limit settings for Admiral container
    Set limits for history versions of photon documents
    
    Change-Id: Ifaae74ec1c89615655a1b31fc14a92f0dcb707c5
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/11627
    Compute-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Sergio Sanchez <sanchezs@vmware.com>

--- host/entrypoint.sh
+++ host/entrypoint.sh
@@ -19,11 +19,15 @@ then
 CONFIG_FILES="$CONFIG_FILES,$CONFIG_FILE_PATH"
 fi
 
+if [ "x" = "x$XENON_PHOTON_MODEL_PROPS" ]
+then
+XENON_PHOTON_MODEL_PROPS="-Dservice.document.version.retention.limit=50 -Dservice.document.version.retention.floor=10"
+fi
 
 JAVA_OPTS="$JAVA_OPTS $MEMORY_OPTS"
 JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
 
-java -Dconfiguration.properties=$CONFIG_FILES $JAVA_OPTS -cp $ADMIRAL_ROOT/*:$ADMIRAL_ROOT/lib/*:/etc/xenon/dynamic-services/* com.vmware.admiral.host.ManagementHost --bindAddress=0.0.0.0 --port=$ADMIRAL_PORT --sandbox=$ADMIRAL_STORAGE_PATH $XENON_OPTS &
+java -Dconfiguration.properties=$CONFIG_FILES $JAVA_OPTS -cp $ADMIRAL_ROOT/*:$ADMIRAL_ROOT/lib/*:/etc/xenon/dynamic-services/* $XENON_PHOTON_MODEL_PROPS com.vmware.admiral.host.ManagementHost --bindAddress=0.0.0.0 --port=$ADMIRAL_PORT --sandbox=$ADMIRAL_STORAGE_PATH $XENON_OPTS &
 PID=$!
 
 wait $PID
