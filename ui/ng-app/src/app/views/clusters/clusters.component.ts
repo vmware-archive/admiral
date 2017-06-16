52b8db9f7b87acaa849c30cfa70d9badc56ccefe
Miroslav Shipkovenski <mshipkovenski@mshipkovenski-m.local>
2017-06-16 17:03:59 +0300

    [UI] Delete Cluster UI
    Change-Id: I6855c1a3446c9fda7de558612541a5fbf7133962
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/11779
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Stanislav Hadjiiski <hadjiiskis@vmware.com>

--- ui/ng-app/src/app/views/clusters/clusters.component.ts
+++ ui/ng-app/src/app/views/clusters/clusters.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Links } from '../../utils/links';
 import { DocumentService } from '../../utils/document.service';
 import * as I18n from 'i18next';
+import { Utils } from '../../utils/utils';
+import { GridViewComponent } from '../../components/grid-view/grid-view.component';
 
 @Component({
   selector: 'app-clusters',
@@ -13,8 +15,39 @@ export class ClustersComponent implements OnInit {
   constructor(private service: DocumentService) { }
 
   serviceEndpoint = Links.CLUSTERS;
+  clusterToDelete: any;
+  deleteConfirmationAlert: string;
+
+  @ViewChild('gridView') gridView:GridViewComponent;
 
   ngOnInit() {
+  }
+
+  get deleteConfirmationDescription(): string {
+    return this.clusterToDelete && this.clusterToDelete.name
+            && I18n.t('clusters.delete.confirmation',
+            { clusterName:  this.clusterToDelete.name } as I18n.TranslationOptions);
+  }
+
+  deleteCluster(event, cluster) {
+    this.clusterToDelete = cluster;
+    event.stopPropagation();
+    return false; // prevents navigation
+  }
+
+  deleteConfirmed() {
+    this.service.delete(this.clusterToDelete.documentSelfLink)
+        .then(result => {
+          this.clusterToDelete = null;
+          this.gridView.refresh();
+        })
+        .catch(err => {
+          this.deleteConfirmationAlert = Utils.getErrorMessage(err)._generic;
+        });
+  }
+
+  deleteCanceled() {
+    this.clusterToDelete = null;
   }
 
   cpuPercentageLevel(cluster) {
