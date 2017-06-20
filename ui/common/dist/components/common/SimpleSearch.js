801748558b62e001b3fc969d9a7ca34563e91dc8
Iveta Ilieva <iilieva@vmware.com>
2017-06-20 11:43:58 +0300

    Fix for simple search component
    Change-Id: I50ea3c1a4a152480506e682a8b72c27754b6cffe
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/11903
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Iveta Ilieva <iilieva@vmware.com>

---
+++ ui/common/dist/components/common/SimpleSearch.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+var $ = require('jquery');
+require('wamda-typeahead');
+var SEARCH_RESULT_LIMIT = 20;
+/**
+ * Component providing simple search typeahead functionality.
+ */
+module.exports = (function () {
+    function SimpleSearch(displayPropertyName, sourceCallback, selectionCallback) {
+        this.$el = $('<div class="simple-search-holder"><input type="text"/></div>');
+        this.$el.find('input').typeahead({ minLength: 0 }, {
+            name: 'simple-search',
+            limit: SEARCH_RESULT_LIMIT,
+            source: sourceCallback,
+            display: displayPropertyName,
+            templates: {
+                suggestion: function (context) {
+                    var name = context.name || '';
+                    var query = context._query || '';
+                    var start = name.indexOf(query);
+                    var end = start + query.length;
+                    var prefix = '';
+                    var suffix = '';
+                    var root = '';
+                    if (start > 0) {
+                        prefix = name.substring(0, start);
+                    }
+                    if (end < name.length) {
+                        suffix = name.substring(end, name.length);
+                    }
+                    root = name.substring(start, end);
+                    return "\n              <div>\n                <div class=\"search-item\">\n                  " + prefix + "<strong>" + root + "</strong>" + suffix + "\n                </div>\n              </div>";
+                },
+                // footer: function(q) {
+                //   if (q.suggestions && q.suggestions.length > 0 && typeaheadSource.lastResult) {
+                //     var i18nOption = {
+                //       count: q.suggestions.length,
+                //       totalCount: typeaheadSource.lastResult.totalCount
+                //     };
+                //     var label = i18n.t('infoMessages.showingCount', i18nOption);
+                //     return `<div class="tt-options-hint">${label}</div>`;
+                //   }
+                // },
+                notFound: function () {
+                    var label = 'Not Found'; // TODO i18n.t('infoMessages.notFound')
+                    return "<div class=\"tt-options-hint\">" + label + "</div>";
+                }
+            }
+        }).on('typeahead:selected', selectionCallback);
+    }
+    SimpleSearch.prototype.getEl = function () {
+        return this.$el[0];
+    };
+    SimpleSearch.prototype.setValue = function (value) {
+        $(this.getEl()).find('input').typeahead('val', value);
+    };
+    SimpleSearch.prototype.getValue = function () {
+        return $(this.getEl()).find('input').typeahead('val');
+    };
+    return SimpleSearch;
+}());
+//# sourceMappingURL=SimpleSearch.js.map
--- ui/common/dist/index.js
+++ ui/common/dist/index.js
@@ -1,6 +1,7 @@
 var RadialProgress = require('./components/common/RadialProgress');
 var NetworkTrafficVisualization = require('./components/common/NetworkTrafficVisualization');
 var Search = require('./components/common/Search');
+var SimpleSearch = require('./components/common/SimpleSearch');
 var formatUtils = require('./core/formatUtils');
 var serviceUtils = require('./core/serviceUtils');
 var searchConstants = require('./core/searchConstants');
@@ -8,6 +9,7 @@ module.exports = {
     RadialProgress: RadialProgress,
     NetworkTrafficVisualization: NetworkTrafficVisualization,
     Search: Search,
+    SimpleSearch: SimpleSearch,
     formatUtils: formatUtils,
     serviceUtils: serviceUtils,
     searchConstants: searchConstants
