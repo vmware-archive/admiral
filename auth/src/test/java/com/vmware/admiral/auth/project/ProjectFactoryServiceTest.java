b29761c3e45c5803e59a006e653293c467b13e20
Angel Ivanov <angeli@vmware.com>
2017-07-18 13:59:34 +0300

    Expose GET access to public projects to not authorized users.
    Change-Id: I1ba8971375cde893f857be72135abe9f73defd40
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/13514
    Compute-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Sergio Sanchez <sanchezs@vmware.com>

--- auth/src/test/java/com/vmware/admiral/auth/project/ProjectFactoryServiceTest.java
+++ auth/src/test/java/com/vmware/admiral/auth/project/ProjectFactoryServiceTest.java
@@ -13,6 +13,7 @@ package com.vmware.admiral.auth.project;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
 import java.util.Collections;
@@ -104,6 +105,34 @@ public class ProjectFactoryServiceTest extends AuthBaseTest {
                     }
                 }).sendWith(host);
         host.testWait();
+    }
+
+    @Test
+    public void testGetPublicProjectOnly() throws Throwable {
+        ProjectState testProject1 = createProject("testProject1", "test", true);
+        ProjectState testProject2 = createProject("testProject2", "test", true);
+        ProjectState testProject3 = createProject("testProject3", "test", false);
+        ProjectState testProject4 = createProject("testProject4", "test", false);
+        assertDocumentExists(testProject1.documentSelfLink);
+        assertDocumentExists(testProject2.documentSelfLink);
+        assertDocumentExists(testProject3.documentSelfLink);
+        assertDocumentExists(testProject4.documentSelfLink);
+
+        host.assumeIdentity(buildUserServicePath(USER_EMAIL_BASIC_USER));
+
+        URI uri = UriUtils.buildUri(ProjectFactoryService.SELF_LINK);
+        uri = UriUtils.extendUriWithQuery(uri, ProjectFactoryService.QUERY_PARAM_PUBLIC, "");
+
+        ServiceDocumentQueryResult getResult = getDocumentNoWait(ServiceDocumentQueryResult.class,
+                uri.toString());
+
+        assertEquals(Long.valueOf(2), getResult.documentCount);
+        assertEquals(2, getResult.documentLinks.size());
+        assertEquals(2, getResult.documents.size());
+        assertTrue(getResult.documentLinks.contains(testProject1.documentSelfLink));
+        assertTrue(getResult.documentLinks.contains(testProject2.documentSelfLink));
+        assertTrue(!getResult.documentLinks.contains(testProject3.documentSelfLink));
+        assertTrue(!getResult.documentLinks.contains(testProject4.documentSelfLink));
     }
 
 }
