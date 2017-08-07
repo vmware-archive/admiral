579753dfc329d62a8cb71cdec4b28bba3ab2f9b6
Stanislav Hadjiiski <hadjiiskis@vmware.com>
2017-08-07 18:23:22 +0300

    [VBV-1549][Harbor] The "Project Repositories" in home page always contains the repositories of default-project
    Removed the hardcoded ID from the sources. Now we are using the index of
    the currently selected project.
    
    Change-Id: I941b68643a1ba43669a25a3ced168dfb62222ebc
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/15191
    Reviewed-by: Miroslav Shipkovenski <mshipkovenski@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>

--- ui/ng-app/src/app/views/hbr/repository/repository.component.ts
+++ ui/ng-app/src/app/views/hbr/repository/repository.component.ts
@@ -1,3 +1,5 @@
+import { Utils } from './../../../utils/utils';
+import { ProjectService } from './../../../utils/project.service';
 import { TagClickEvent } from 'harbor-ui';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
@@ -17,10 +19,21 @@ import { Component } from '@angular/core';
   `
 })
 export class RepositoryComponent {
-  projectId = 1;
+
+  private static readonly HBR_DEFAULT_PROJECT_INDEX: Number = 1;
+  private static readonly CUSTOM_PROP_PROJECT_INDEX: string = '__projectIndex';
+
   sessionInfo = {};
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private ps: ProjectService) {}
+
+  get projectId(): Number {
+    let selectedProject = this.ps && this.ps.getSelectedProject();
+    let projectIndex = selectedProject && Utils.getCustomPropertyValue(
+      selectedProject.customProperties,
+      RepositoryComponent.CUSTOM_PROP_PROJECT_INDEX);
+    return (projectIndex && Number(projectIndex)) || RepositoryComponent.HBR_DEFAULT_PROJECT_INDEX;
+  }
 
   watchTagClickEvent(tag: TagClickEvent) {
     this.router.navigate(['repositories', tag.repository_name, 'tags', tag.tag_name], {relativeTo: this.route});
