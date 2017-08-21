bbeffa9dbfe628e1cb9aeccb941f676e9a8dc2b3
Angel Ivanov <angeli@vmware.com>
2017-08-21 14:06:49 +0300

    Show link to download harbor certificate in configuration view.
    Currently link will be visible only for cloud admin role.
    
    Change-Id: I4a6d257f814d28702fe2605135779fcbe5ea8c2b
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/15917
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Stanislav Hadjiiski <hadjiiskis@vmware.com>

--- ui/ng-app/src/app/views/configuration/configuration.component.ts
+++ ui/ng-app/src/app/views/configuration/configuration.component.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+import { Roles } from './../../utils/roles';
+import { AuthService } from './../../utils/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +20,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigurationComponent implements OnInit {
 
-  constructor() { }
+  hasAdminRole: boolean;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) { 
+    
   }
 
+  ngOnInit() {
+    this.setIsAdmin();
+  }
+
+  private setIsAdmin() {
+    
+    this.authService.getCachedSecurityContext().then(securityContext => {
+      let isAdmin = false;
+      if (securityContext && securityContext.roles) {
+        for (var index = 0; index < securityContext.roles.length; index++) {
+          var role = securityContext.roles[index];
+          if (Roles.CLOUD_ADMIN === role) {
+            isAdmin = true;
+            break;
+          }
+        }
+      }
+      this.hasAdminRole = isAdmin;
+    });
+
+  }
 }
