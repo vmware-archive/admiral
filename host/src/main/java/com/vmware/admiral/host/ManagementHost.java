6ebe84461bbf967f77aa6b111d151d41287820a3
Sergio Sanchez <sanchezs@vmware.com>
2017-09-15 10:08:30 +0300

    Fixes #205 - Harden Admiral TLS configuration
    Disabled the support for TLS v1.0 by default due to the BEAST
    vulnerability.
    See https://en.wikipedia.org/wiki/Transport_Layer_Security#BEAST_attack
    
    nmap scan before the fix:
    $ nmap --script ssl-enum-ciphers -p 8282 localhost
    
    Starting Nmap 7.40 ( https://nmap.org ) at 2017-09-15 10:02 EEST
    Nmap scan report for localhost (127.0.0.1)
    Host is up (0.00026s latency).
    Other addresses for localhost (not scanned): ::1
    PORT     STATE SERVICE
    8282/tcp open  libelle
    | ssl-enum-ciphers:
    |   TLSv1.0:
    |     ciphers:
    |       TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA (secp160k1) - A
    |       TLS_RSA_WITH_AES_128_CBC_SHA (rsa 2048) - A
    |     compressors:
    |       NULL
    |     cipher preference: client
    |     warnings:
    |       Key exchange (secp160k1) of lower strength than certificate key
    |   TLSv1.1:
    |     ciphers:
    |       TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA (secp160k1) - A
    |       TLS_RSA_WITH_AES_128_CBC_SHA (rsa 2048) - A
    |     compressors:
    |       NULL
    |     cipher preference: client
    |     warnings:
    |       Key exchange (secp160k1) of lower strength than certificate key
    |   TLSv1.2:
    |     ciphers:
    |       TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA (secp160k1) - A
    |       TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 (secp160k1) - A
    |       TLS_RSA_WITH_AES_128_CBC_SHA (rsa 2048) - A
    |       TLS_RSA_WITH_AES_128_GCM_SHA256 (rsa 2048) - A
    |     compressors:
    |       NULL
    |     cipher preference: client
    |     warnings:
    |       Key exchange (secp160k1) of lower strength than certificate key
    |_  least strength: A
    
    Nmap done: 1 IP address (1 host up) scanned in 0.78 seconds
    
    nmap scan after the fix:
    $ nmap --script ssl-enum-ciphers -p 8282 localhost
    
    Starting Nmap 7.40 ( https://nmap.org ) at 2017-09-15 10:04 EEST
    Nmap scan report for localhost (127.0.0.1)
    Host is up (0.00021s latency).
    Other addresses for localhost (not scanned): ::1
    PORT     STATE SERVICE
    8282/tcp open  libelle
    | ssl-enum-ciphers:
    |   TLSv1.1:
    |     ciphers:
    |       TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA (secp160k1) - A
    |       TLS_RSA_WITH_AES_128_CBC_SHA (rsa 2048) - A
    |     compressors:
    |       NULL
    |     cipher preference: client
    |     warnings:
    |       Key exchange (secp160k1) of lower strength than certificate key
    |   TLSv1.2:
    |     ciphers:
    |       TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA (secp160k1) - A
    |       TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 (secp160k1) - A
    |       TLS_RSA_WITH_AES_128_CBC_SHA (rsa 2048) - A
    |       TLS_RSA_WITH_AES_128_GCM_SHA256 (rsa 2048) - A
    |     compressors:
    |       NULL
    |     cipher preference: client
    |     warnings:
    |       Key exchange (secp160k1) of lower strength than certificate key
    |_  least strength: A
    
    Nmap done: 1 IP address (1 host up) scanned in 0.75 seconds
    
    Change-Id: I9450cbfbe6dc5456077e6346892c84ebcab20a25
    Reviewed-on: http://bellevue-ci.eng.vmware.com:8080/17135
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Georgi Muleshkov <gmuleshkov@vmware.com>

--- host/src/main/java/com/vmware/admiral/host/ManagementHost.java
+++ host/src/main/java/com/vmware/admiral/host/ManagementHost.java
@@ -14,6 +14,7 @@ package com.vmware.admiral.host;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.security.Security;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.logging.Level;
@@ -78,6 +79,12 @@ public class ManagementHost extends ServiceHost implements IExtensibilityRegistr
         if (System.getProperty("service.document.version.retention.floor") == null) {
             System.setProperty("service.document.version.retention.floor", "2");
         }
+
+        // Disable TLS v1.0 by default due to the BEAST vulnerability.
+        // (see https://en.wikipedia.org/wiki/Transport_Layer_Security#BEAST_attack)
+        String disabledAlgorithms = Security.getProperty("jdk.tls.disabledAlgorithms");
+        disabledAlgorithms = "TLSv1, " + disabledAlgorithms;
+        Security.setProperty("jdk.tls.disabledAlgorithms", disabledAlgorithms);
     }
 
     /**
