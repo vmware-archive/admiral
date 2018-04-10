1011794678a96e1fd78895677c8f88cadf23ba0c
Miroslav Shipkovenski <mshipkovenski@vmware.com>
2018-04-10 17:45:58 +0300

    [UI] K8s nodes view
    Change-Id: I2422d658a0cc8ed6cc1b7204761a0dc92cbc16e5
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/31036
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Antonio Filipov <afilipov@vmware.com>

--- ui/ng-app/src/app/views/kubernetes/clusters/details/kubernetes-cluster-nodes.component.ts
+++ ui/ng-app/src/app/views/kubernetes/clusters/details/kubernetes-cluster-nodes.component.ts
@@ -9,7 +9,7 @@
  * conditions of the subcomponent's license, as noted in the LICENSE file.
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AutoRefreshComponent } from "../../../../components/base/auto-refresh.component";
 import { DocumentService } from "../../../../utils/document.service";
@@ -24,8 +24,27 @@ import { Utils } from "../../../../utils/utils";
 /**
 *  A k8s cluster nodes view.
 */
-export class KubernetesClusterNodesComponent {
+export class KubernetesClusterNodesComponent implements OnChanges {
     @Input() cluster: any;
 
-    constructor(private service: DocumentService) {}
+    nodes: any[];
+
+    constructor(private service: DocumentService) { }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (!this.cluster) {
+            return;
+        }
+        var nodeLink = this.cluster.nodeLinks[0];
+        var nodesJson = Utils.getCustomPropertyValue(this.cluster.nodes[nodeLink].customProperties, '__nodes');
+        if (!nodesJson) {
+            return;
+        }
+        var nodes = JSON.parse(nodesJson);
+        this.nodes = nodes.map(n => ({
+            name: n.name,
+            cpu: Math.floor(n.usedCPU),
+            memory: Math.floor(parseFloat(n.availableMem) / parseFloat(n.totalMem) * 100)
+        }));
+    }
 }
