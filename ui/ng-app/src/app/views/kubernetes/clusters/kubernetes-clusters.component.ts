9b3f2ce10136a87fbc00c06f1cf1deb52753c025
Alek Boninski <boninskia@vmware.com>
2018-04-17 16:40:53 +0300

    Enable/Disable k8s clusters
    Change-Id: Ib833e1be078958f811bd0d9e76e24c149a54e052
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/31473
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Miroslav Shipkovenski <mshipkovenski@vmware.com>

--- ui/ng-app/src/app/views/kubernetes/clusters/kubernetes-clusters.component.ts
+++ ui/ng-app/src/app/views/kubernetes/clusters/kubernetes-clusters.component.ts
@@ -9,9 +9,11 @@
  * conditions of the subcomponent's license, as noted in the LICENSE file.
  */
 
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DocumentService } from '../../../utils/document.service';
+import { Constants } from '../../../utils/constants';
+import { GridViewComponent } from '../../../components/grid-view/grid-view.component';
 import { ProjectService } from "../../../utils/project.service";
 import { AutoRefreshComponent } from '../../../components/base/auto-refresh.component';
 import { FT } from '../../../utils/ft';
@@ -29,7 +31,10 @@ import * as I18n from 'i18next';
  */
 export class KubernetesClustersComponent extends AutoRefreshComponent {
 
+    @ViewChild('gridView') gridView: GridViewComponent;
+
     serviceEndpoint = Links.CLUSTERS + '?type=KUBERNETES';
+    selectedItem: any;
 
     constructor(protected service: DocumentService, protected projectService: ProjectService,
         protected router: Router, protected route: ActivatedRoute) {
@@ -49,4 +54,68 @@ export class KubernetesClustersComponent extends AutoRefreshComponent {
         return I18n.t('clusters.state.' + cluster.status);
     }
 
+    operationSupported(op, cluster) {
+        if (op === 'ENABLE') {
+            return cluster.status === Constants.clusters.status.DISABLED;
+        } else if (op === 'DISABLE') {
+            return cluster.status === Constants.clusters.status.ON;
+        }
+
+        return true;
+    }
+
+    enableHost(event, cluster) {
+        event.stopPropagation();
+
+        if (!cluster.nodeLinks || cluster.nodeLinks.length < 1) {
+            return;
+        }
+
+        var hostLink = cluster.nodeLinks[0];
+
+        this.service.patch(hostLink, {'powerState': Constants.hosts.state.ON})
+        .then(result => {
+            this.gridView.refresh();
+        })
+        .catch(err => {
+            console.log(Utils.getErrorMessage(err)._generic);
+        });
+
+        return false; // prevents navigation
+    }
+
+    disableHost(event, cluster) {
+        event.stopPropagation();
+
+        if (!cluster.nodeLinks || cluster.nodeLinks.length < 1) {
+            return;
+        }
+
+        var hostLink = cluster.nodeLinks[0];
+
+        this.service.patch(hostLink, {'powerState': Constants.hosts.state.SUSPEND})
+        .then(result => {
+            this.gridView.refresh();
+        })
+        .catch(err => {
+            console.log(Utils.getErrorMessage(err)._generic);
+        });
+
+        return false; // prevents navigation
+    }
+
+    isItemSelected(item: any) {
+        return item === this.selectedItem;
+    }
+
+    toggleItemSelection($event, item) {
+        $event.stopPropagation();
+
+        if (this.isItemSelected(item)) {
+            // clear selection
+            this.selectedItem = null;
+        } else {
+            this.selectedItem = item;
+        }
+    }
 }
