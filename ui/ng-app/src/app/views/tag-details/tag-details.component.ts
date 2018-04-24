96deb6da09ec39edb3609f50a7c9755a31c6666a
Iveta Ilieva <iilieva@vmware.com>
2018-04-24 13:13:55 +0300

    Fix for Harbor tag details view Back navigation.
    Change-Id: I8cd76957cd975d591af3957d56602c4cf216d68d
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/31990
    Bellevue-Verified: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>
    Closures-Verified: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>
    Reviewed-by: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>
    CS-Verified: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>
    Upgrade-Verified: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>
    Tested-by: e_vcoauto_glob_1 <e_vcoauto_glob_1@vmware.com>

--- ui/ng-app/src/app/views/tag-details/tag-details.component.ts
+++ ui/ng-app/src/app/views/tag-details/tag-details.component.ts
@@ -1,25 +1,38 @@
-import { Route, ActivatedRoute, Router } from '@angular/router';
+/*
+ * Copyright (c) 2017-2018 VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with separate copyright notices
+ * and license terms. Your use of these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'app-tag-details',
-  templateUrl: './tag-details.component.html',
-  styleUrls: ['./tag-details.component.scss']
+    selector: 'app-tag-details',
+    templateUrl: './tag-details.component.html',
+    styleUrls: ['./tag-details.component.scss']
 })
 export class TagDetailsComponent implements OnInit {
 
-  tagId: string;
-  repositoryId: string;
+    tagId: string;
+    repositoryId: string;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+    constructor(private route: ActivatedRoute, private router: Router) {
+    }
 
-  ngOnInit() {
-    this.repositoryId = this.route.snapshot.params['rid'];
-    this.tagId = this.route.snapshot.params['tid'];
-  }
+    ngOnInit() {
+        this.repositoryId = this.route.snapshot.params['rid'];
+        this.tagId = this.route.snapshot.params['tid'];
+    }
 
-  goBack(tag: string) {
-    this.router.navigate(['../../../../'], {relativeTo: this.route});
-  }
+    goBack(tag: string) {
+        this.router.navigate(['../../../../'], {relativeTo: this.route});
+    }
 
 }
