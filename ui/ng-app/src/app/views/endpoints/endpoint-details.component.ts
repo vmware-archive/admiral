3c651863fce71450541fe002c718c0bbb1791b08
Iveta Ilieva <iilieva@vmware.com>
2018-05-09 15:57:33 +0300

    Enhancements for endpoint create/edit view.
    Change-Id: I23811b83f89edfc507a22b3b30f7f9c8eacadf3c
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/33215
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Iveta Ilieva <iilieva@vmware.com>

--- ui/ng-app/src/app/views/endpoints/endpoint-details.component.ts
+++ ui/ng-app/src/app/views/endpoints/endpoint-details.component.ts
@@ -11,16 +11,21 @@
 
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-import { FormControl, FormGroup } from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import { BaseDetailsComponent } from '../../components/base/base-details.component';
 import { DocumentService } from "../../utils/document.service";
 import { ErrorService } from "../../utils/error.service";
+import { Constants } from "../../utils/constants";
 import { Links } from "../../utils/links";
+import { Utils } from "../../utils/utils";
+
+import * as I18n from 'i18next';
 
 
 @Component({
     selector: 'app-endpoint-details',
-    templateUrl: './endpoint-details.component.html'
+    templateUrl: './endpoint-details.component.html',
+    styleUrls: ['./endpoint-details.component.scss']
 })
 /**
  * View for endpoint creation.
@@ -28,18 +33,31 @@ import { Links } from "../../utils/links";
 export class EndpointDetailsComponent extends BaseDetailsComponent {
     editMode: boolean = false;
 
+    credentials: any[];
+
     endpointDetailsForm = new FormGroup({
-        name: new FormControl(''),
+        name: new FormControl('', Validators.required),
         description: new FormControl(''),
-        address: new FormControl(''),
-        userName: new FormControl(''),
-        password: new FormControl('')
+        uaaAddress: new FormControl('', Validators.required),
+        uaaCredentials: new FormControl('', Validators.required),
+        pksAddress: new FormControl('', Validators.required)
     });
 
     isSavingEndpoint: boolean = false;
     isTestingConnection: boolean = false;
 
+    alertType: any;
     alertMessage: string;
+
+    credentialsTitle = I18n.t('dropdownSearchMenu.title', {
+        ns: 'base',
+        entity: I18n.t('app.credential.entity', {ns: 'base'})
+    } as I18n.TranslationOptions );
+
+    credentialsSearchPlaceholder = I18n.t('dropdownSearchMenu.searchPlaceholder', {
+        ns: 'base',
+        entity: I18n.t('app.credential.entity', {ns: 'base'})
+    } as I18n.TranslationOptions );
 
     constructor(route: ActivatedRoute, documentService: DocumentService, router: Router,
                 errorService: ErrorService) {
@@ -50,22 +68,91 @@ export class EndpointDetailsComponent extends BaseDetailsComponent {
         if (this.entity) {
             // edit mode
             this.editMode = true;
+            // Name
+            this.endpointDetailsForm.get('name').setValue(this.entity.name);
+            // Description
+            if (this.entity.details) {
+                this.endpointDetailsForm.get('description').setValue(this.entity.details);
+            }
+
+            // TODO uaa and pks address population
+
+            // TODO credentials population
+            let authCredentialsLink =
+                Utils.getCustomPropertyValue(this.entity.customProperties, '__authCredentialsLink');
+            if (authCredentialsLink) {
+                let credItem = this.credentials
+                                    .filter((c) => c.documentSelfLink === authCredentialsLink);
+                if (credItem.length > 0) {
+                    this.endpointDetailsForm.get('uaaCredentials').setValue(credItem[0]);
+                }
+            }
         }
     }
 
-    create() {
+    ngOnInit() {
+        super.ngOnInit();
 
+        this.populateCredentials();
+    }
+
+    populateCredentials() {
+        if (this.credentials) {
+            return;
+        }
+
+        this.service.list(Links.CREDENTIALS, {}).then(credentials => {
+            this.credentials = credentials.documents
+                                    .filter(c => !Utils.areSystemScopedCredentials(c))
+                                    .map(Utils.toCredentialViewModel);
+        }).catch((e) => {
+            console.log('Credentials retrieval failed', e);
+        });
+    }
+
+    create() {
+        console.log('create');
+
+        if (this.endpointDetailsForm.valid) {
+            // TODO - if success -> this.goBack(); else show err, stay on same screen
+            this.isSavingEndpoint = true;
+
+        }
     }
 
     save() {
+        console.log('save');
 
+        if (this.endpointDetailsForm.valid) {
+            // TODO - if success -> this.goBack(); else show err, stay on same screen
+            // this.isSavingEndpoint = true;
+
+        }
     }
 
     testConnection() {
+        console.log('test connection');
 
+        if (this.endpointDetailsForm.valid) {
+            // TODO - if success show green message - connection success, else show red err message
+            // this.isTestingConnection = true;
+            //
+            // this.alertType = Constants.alert.type.SUCCESS;
+            // this.alertMessage = I18n.t('endpoint.connectionVerified');
+
+        }
+    }
+
+    cancel() {
+        this.goBack();
+    }
+
+    goBack() {
+        this.router.navigate(['../'], {relativeTo: this.route});
     }
 
     resetAlert() {
+        this.alertType = null;
         this.alertMessage = null;
     }
 }
