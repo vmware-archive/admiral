310a4dc83ef0f4a7982308336e9b61bfb67443b6
Lazarin Lazarov <llazarov@vmware.com>
2018-06-21 16:16:49 +0300

    Disable debugging mode
    Change-Id: I9cf5cd853baf1fcef36a6bf4e7294f17d59d1cd2
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/36827
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Lazarin Lazarov <llazarov@vmware.com>

--- host/src/main/java/com/vmware/admiral/host/ManagementHost.java
+++ host/src/main/java/com/vmware/admiral/host/ManagementHost.java
@@ -154,9 +154,6 @@ public class ManagementHost extends ServiceHost implements IExtensibilityRegistr
         log(Level.INFO, "Initializing ...");
         initialize(args);
 
-        //TODO remove the debugging mode
-        toggleDebuggingMode(true);
-
         log(Level.INFO, "Registering service interceptors ...");
         registerOperationInterceptors();
 
