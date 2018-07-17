a8fd750d8309c7ea77567ef96856e0a575282828
Antonio Filipov <afilipov@vmware.com>
2018-07-17 11:33:32 +0300

    Fix for jasmine tests
    - increased browserDisconnectTolerance from 1 to 3
    
    Change-Id: I3794eed1f27f5bc4de39ef85ffcf40f06d962b71
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/38912
    Closures-Verified: jenkins <jenkins@vmware.com>
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Antonio Filipov <afilipov@vmware.com>

--- ui/app/gulp/tasks/tests.js
+++ ui/app/gulp/tasks/tests.js
@@ -65,9 +65,9 @@ var createCommonTestsConfig =  function(files, reporters, reportOutputFile, sing
     singleRun: singleRun,
     globals: config.INTEGRATION_TEST_PROPERTIES,
     captureTimeout: 60000,
-    browserDisconnectTimeout : 10000,
-    browserDisconnectTolerance : 1,
-    browserNoActivityTimeout : 60000,
+    browserDisconnectTimeout : 60000,
+    browserDisconnectTolerance : 3,
+    browserNoActivityTimeout : 60000
   }
 };
 
