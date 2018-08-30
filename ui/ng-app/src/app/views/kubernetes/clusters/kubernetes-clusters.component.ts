daf599eb99893e1dbc801a7c53b1550238f4ddc3
Petya Aleksandrova <petyaa@vmware.com>
2018-08-30 15:27:33 +0300

    Minor UI fixes
    Change-Id: Ifdad89d71fa8a3d219fa785a05eac2174f594d2d
    Reviewed-on: https://bellevue-ci.eng.vmware.com:8080/43212
    Upgrade-Verified: jenkins <jenkins@vmware.com>
    Closures-Verified: jenkins <jenkins@vmware.com>
    Bellevue-Verified: jenkins <jenkins@vmware.com>
    CS-Verified: jenkins <jenkins@vmware.com>
    Reviewed-by: Iveta Ilieva <iilieva@vmware.com>

--- ui/ng-app/src/app/views/kubernetes/clusters/kubernetes-clusters.component.ts
+++ ui/ng-app/src/app/views/kubernetes/clusters/kubernetes-clusters.component.ts
@@ -106,10 +106,15 @@ export class KubernetesClustersComponent extends AutoRefreshComponent {
     }
 
     get deleteOpTitle() {
+        let key = this.deleteOp === 'REMOVE'
+                    ? 'kubernetes.clusters.remove.title'
+                    : 'kubernetes.clusters.destroy.title';
+
         return this.deleteOpClusterName
-            && (this.deleteOp === 'REMOVE'
-                    ? I18n.t('kubernetes.clusters.remove.title')
-                    : I18n.t('kubernetes.clusters.destroy.title'));
+            && I18n.t(key, {
+                clusterName: this.deleteOpClusterName,
+                interpolation: {escapeValue: false}
+            } as I18n.TranslationOptions);
     }
 
     get deleteOpConfirmationDescription(): string {
@@ -118,18 +123,14 @@ export class KubernetesClustersComponent extends AutoRefreshComponent {
             return description;
         }
 
-        if (this.deleteOp === 'REMOVE') {
-            description = I18n.t('kubernetes.clusters.remove.confirmation', {
-                clusterName: this.deleteOpClusterName,
-                interpolation: { escapeValue: false }
-            } as I18n.TranslationOptions)
+        let key = this.deleteOp === 'REMOVE'
+                    ? 'kubernetes.clusters.remove.confirmation'
+                    : 'kubernetes.clusters.destroy.confirmation';
 
-        } else if (this.deleteOp === 'DESTROY') {
-            description = I18n.t('kubernetes.clusters.destroy.confirmation', {
-                clusterName: this.deleteOpClusterName,
-                interpolation: { escapeValue: false }
-            } as I18n.TranslationOptions);
-        }
+        description = I18n.t(key, {
+            clusterName: this.deleteOpClusterName,
+            interpolation: { escapeValue: false }
+        } as I18n.TranslationOptions);
 
         return description;
     }
